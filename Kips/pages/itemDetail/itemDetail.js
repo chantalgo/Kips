33f21cb0977418107d9358523ba66a618fada7be
Andrew Altamirano <slogen123@gmail.com>
2013-05-18 01:19:12 -0400

    revert item page title to group name

--- Kips/pages/itemDetail/itemDetail.js
+++ Kips/pages/itemDetail/itemDetail.js
@@ -7,31 +7,30 @@
         ready: function (element, options) {
             var item = options.item;
 
-            element.querySelector(".titlearea .pagetitle").textContent = item.title;
-
             var timeago = new Date(item.pubDate * 1000).timeSince();
             if (timeago != undefined) {
                 element.querySelector(".titlearea .postdate").textContent = timeago;
                 element.querySelector(".titlearea .postdate").setAttribute('datetime', item.pubDate);
             }
             else {
-                element.querySelector(".titlearea .postdate").removeNode();
+                element.querySelector(".titlearea .pageinfo").removeNode();
             }
 
             if (item.group) {
-                element.querySelector(".titlearea .postlocation").textContent = item.group.title;
+                element.querySelector(".titlearea .pagetitle").textContent = item.group.title;
             }
             else if (options.via) {
                 switch(options.via){
                     case "feed":
-                        element.querySelector(".titlearea .postlocation").textContent = "Feed";
+                        element.querySelector(".titlearea .pagetitle").textContent = "Feed";
                         break;
                     case "favorites":
-                        element.querySelector(".titlearea .postlocation").textContent = "Favorites";
+                        element.querySelector(".titlearea .pagetitle").textContent = "Favorites";
                         break;
                 }
             }
 
+            element.querySelector("article .item-title").textContent = item.title;
             element.querySelector("article .item-url").textContent = item.url;
             element.querySelector("article .item-url").href = item.url;
             element.querySelector("article .item-url").title = item.title;
