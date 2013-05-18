50a723b6acba43fcc5a89ec383a187da257d935b
Andrew Altamirano <slogen123@gmail.com>
2013-05-18 01:06:03 -0400

    item detail styling

--- Kips/pages/itemDetail/itemDetail.js
+++ Kips/pages/itemDetail/itemDetail.js
@@ -6,23 +6,54 @@
         // populates the page elements with the app's data.
         ready: function (element, options) {
             var item = options.item;
+
+            element.querySelector(".titlearea .pagetitle").textContent = item.title;
+
+            var timeago = new Date(item.pubDate * 1000).timeSince();
+            if (timeago != undefined) {
+                element.querySelector(".titlearea .postdate").textContent = timeago;
+                element.querySelector(".titlearea .postdate").setAttribute('datetime', item.pubDate);
+            }
+            else {
+                element.querySelector(".titlearea .postdate").removeNode();
+            }
+
             if (item.group) {
-                element.querySelector(".titlearea .pagetitle").textContent = item.group.title;
+                element.querySelector(".titlearea .postlocation").textContent = item.group.title;
             }
             else if (options.via) {
                 switch(options.via){
                     case "feed":
-                        element.querySelector(".titlearea .pagetitle").textContent = "Feed";
+                        element.querySelector(".titlearea .postlocation").textContent = "Feed";
                         break;
                     case "favorites":
-                        element.querySelector(".titlearea .pagetitle").textContent = "Favorites";
+                        element.querySelector(".titlearea .postlocation").textContent = "Favorites";
                         break;
                 }
             }
-            element.querySelector("article .item-title").textContent = item.title;
-            element.querySelector("article .item-subtitle").textContent = item.url;
-            element.querySelector("article .item-image").src = item.backgroundImage;
-            element.querySelector("article .item-image").alt = item.content;
+
+            element.querySelector("article .item-url").textContent = item.url;
+            element.querySelector("article .item-url").href = item.url;
+            element.querySelector("article .item-url").title = item.title;
+
+            if (item.backgroundImage != null) {
+                element.querySelector("article .item-image").src = item.backgroundImage;
+                element.querySelector("article .item-image").alt = item.content;
+            }
+            else {
+                element.querySelector("article .item-image").removeNode();
+            }
+
+            if (item.notes != null) {
+                var markdown = document.createElement('div');
+                markdown.innerHTML = new Showdown.converter().makeHtml(item.notes);
+                element.querySelector("article .notes-content").appendChild(markdown);
+            }
+            else {
+                element.querySelector("article .notes-content").removeNode();
+            }
+
+            
             element.querySelector("article .item-content").innerHTML = item.content;
             element.querySelector(".content").focus();
 
